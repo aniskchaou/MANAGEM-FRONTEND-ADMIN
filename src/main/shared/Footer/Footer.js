ed4ad616882803305ed11d538f90d0ec340dbe2c
Anis KCHAOU <kchaouanis25@gmail.com>
2022-08-22 09:24:09 +0200

    Update Footer.js

--- src/main/shared/Footer/Footer.js
+++ src/main/shared/Footer/Footer.js
@@ -11,7 +11,7 @@ const Footer = () => (
     <div className="footer-inner bg-white">
       <div className="row">
         <div className="col-sm-6">
-          Developed by <a href="https://github.com/aniskchaou">Anis KCHAOU</a>
+        
         </div>
         <div className="col-sm-6 text-right">
           Designed by <a href="https://colorlib.com">Colorlib</a>
